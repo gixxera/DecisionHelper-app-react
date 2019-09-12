5a33319a8643e7dfa66ac92b81c2026c470be741
Milen <monioo@abv.bg>
2019-09-12 17:15:20 +0300

    Convert components to use class properties instead of manually binding this

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,34 @@ import Action from './Action';
 import Header from './Header';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.state = {
-      options: []
+  state = {
+    options: []
+  };
+
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: [] }));
+  }
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option)
+    }));
+  }
+
+  handlePick = () => {
+    const randomIndex = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomIndex]
+    alert(option);
+  }
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'You need to provide an option'
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists'
     }
+
+    this.setState((prevState) => ({ options: prevState.options.concat([option]) }));
   }
 
   componentDidMount() {
@@ -34,32 +53,6 @@ class App extends React.Component {
       const json = JSON.stringify(this.state.options);
       localStorage.setItem('options', json);
     }
-  }
-
-  handleDeleteOptions() {
-    this.setState(() => ({ options: [] }));
-  }
-
-  handleDeleteOption(optionToRemove) {
-    this.setState((prevState) => ({
-      options: prevState.options.filter((option) => optionToRemove !== option)
-    }));
-  }
-
-  handlePick() {
-    const randomIndex = Math.floor(Math.random() * this.state.options.length);
-    const option = this.state.options[randomIndex]
-    alert(option);
-  }
-
-  handleAddOption(option) {
-    if (!option) {
-      return 'You need to provide an option'
-    } else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists'
-    }
-
-    this.setState((prevState) => ({ options: prevState.options.concat([option]) }));
   }
 
   render() {

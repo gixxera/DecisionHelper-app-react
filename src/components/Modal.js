573d3906c6be4d4f6ee066dd09f45fafea143d92
Milen <monioo@abv.bg>
2019-09-13 17:27:59 +0300

    Style the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-  <Modal onRequestClose={props.handleClearOption} ariaHideApp={false} isOpen={!!props.selectedOption} contentLabel="Selected Option">
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearOption}>Cancel</button>
+  <Modal className="modal" closeTimeoutMS={200} onRequestClose={props.handleClearOption} ariaHideApp={false} isOpen={!!props.selectedOption} contentLabel="Selected Option">
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="btn" onClick={props.handleClearOption}>Cancel</button>
   </Modal>
 );
 
--- src/components/Options.js
+++ src/components/Options.js
@@ -7,7 +7,7 @@ const Options = (props) => (
       <h3 className="widget-header__title">Your Options</h3>
       <button className="btn btn--link" onClick={props.handleDeleteOptions}>Remove All</button>
     </div>
-    {props.options.length === 0 && <p class="info-paragraph">Please add an option</p>}
+    {props.options.length === 0 && <p className="info-paragraph">Please add an option</p>}
     {
       props.options.map((option, index) => <Option count={index + 1} key={option} optionText={option} handleDeleteOption={props.handleDeleteOption} />)
     }
